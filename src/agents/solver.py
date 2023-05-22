09695bcea0a85dead67aebec90210b7150d6010f
Dan <guitarman.dan29@gmail.com>
2023-05-22 16:29:45 -0400

    added cplex, matching and networkx mvc solve

--- src/agents/solver.py
+++ src/agents/solver.py
@@ -1,8 +1,12 @@
 from abc import ABC, abstractmethod
 
 from src.envs.spinsystem import SpinSystemBase
+from src.envs.utils import OptimisationTarget
 import numpy as np
+import networkx as nx
 import torch
+import random
+import cplex
 
 class SpinSolver(ABC):
     """Abstract base class for agents solving SpinSystem Ising problems."""
@@ -24,7 +28,7 @@ class SpinSolver(ABC):
 
         self.env = env
         self.verbose = verbose
-        self.record_cut = record_cut
+        self.record_solution = record_cut
         self.record_rewards = record_rewards
         self.record_qs = record_qs
 
@@ -89,6 +93,10 @@ class Greedy(SpinSolver):
 
         super().__init__(*args, **kwargs)
 
+    def reset(self, spins = None):
+        super().reset()
+        self.env.reset(spins)
+
     def step(self):
         """Take the action which maximises the immediate reward.
 
@@ -97,18 +105,18 @@ class Greedy(SpinSolver):
             done (bool): Whether the environment is in a terminal state after
                 the action is taken.
         """
-        rewards_avaialable = self.env.get_immediate_rewards_available()
+        rewards_available = self.env.scorer.get_score_mask(self.env.state[0, :self.env.n_spins], self.env.matrix)
 
         if self.env.reversible_spins:
-            action = rewards_avaialable.argmax()
+            action = rewards_available.argmax()
         else:
-            masked_rewards_avaialable = rewards_avaialable.copy()
+            masked_rewards_avaialable = rewards_available.copy()
             np.putmask(masked_rewards_avaialable,
                        self.env.get_observation()[0, :] != self.env.get_allowed_action_states(),
                        -100)
             action = masked_rewards_avaialable.argmax()
 
-        if rewards_avaialable[action] < 0:
+        if rewards_available[action] < 0:
             action = None
             reward = 0
             done = True
@@ -119,6 +127,19 @@ class Greedy(SpinSolver):
 
 class Random(SpinSolver):
     """A random solver for a SpinSystem."""
+
+    def __init__(self, *args, **kwargs):
+        """Initialise a random solver.
+
+        Args:
+            *args: Passed through to the SpinSolver constructor.
+
+        Attributes:
+            trial_env (SpinSystemMCTS): The environment with in the agent tests
+                actions (a clone of self.env where the final actions are taken).
+            current_snap: The current state of the environment.
+        """
+        super().__init__(*args, **kwargs)
 
     def step(self):
         """Take a random action.
@@ -199,19 +220,145 @@ class Network(SpinSolver):
             reward = 0
             done = True
 
-        if not self.record_cut and not self.record_rewards:
+        if not self.record_solution and not self.record_rewards:
             record = [action]
         else:
             record = [action]
-            if self.record_cut:
-                record += [self.env.calculate_cut()]
+            if self.record_solution:
+                record += [self.env.scorer.get_solution(self.env.state[0, :self.env.n_spins], self.env.matrix)]
             if self.record_rewards:
                 record += [reward]
             if self.record_qs:
                 record += [qs]
 
-        record += [self.env.get_immediate_rewards_available()]
+        record += [self.env.scorer.get_score_mask(self.env.state[0, :self.env.n_spins], self.env.matrix)]
 
         self.history.append(record)
 
         return reward, done
+
+
+class CoverMatching(SpinSolver):
+    def __init__(self, env: SpinSystemBase, record_cut=False, record_rewards=False, record_qs=False, verbose=False):
+        super().__init__(env, record_cut, record_rewards, record_qs, verbose)
+
+    def reset(self):
+        """
+        Reset the environment to an empty solution
+        """
+        super().reset()
+        ### Cover matching always starts with an empty solution
+        self.total_reward = 0
+        self.env.reset([-1] * self.env.n_spins)
+
+    def step(self):
+        rew = 0 ### Because the solve function needs this
+        spins = self.env.state[0, :self.env.n_spins]
+        matrix = self.env.matrix
+
+        ### Matrix containing only the uncovered edges
+        edges = matrix * np.array([spins == -1]) * np.array([spins == -1]).T
+
+        ### Gets x and y coordinates (i.e. node index) of the nodes incident on uncovered edges
+        coordinates = list(zip(*np.where(edges == 1)))
+
+        ### Gets the two nodes of the chosen edge
+        chosen_edge = random.choice(coordinates)
+        ### Make the action for both nodes to add them to the solution
+        for vertex in chosen_edge:
+            observation, reward, done, _ = self.env.step(vertex)
+            rew += reward
+
+        ### Now if we have a valid solution, we're done
+        new_spins = self.env.state[0, :self.env.n_spins]
+        ### if every single entry is a 0
+        if np.all(matrix * np.array([new_spins == -1]) * np.array([new_spins == -1]).T == 0):
+            done = True
+
+        return rew, done
+
+
+class CplexSolver(SpinSolver):
+    def __init__(self, env: SpinSystemBase, record_cut=False, record_rewards=False, record_qs=False, verbose=False):
+        super().__init__(env, record_cut, record_rewards, record_qs, verbose)
+        self._solver = cplex.Cplex()
+
+    def reset(self):
+        """
+        Reset the environment to an empty solution and prepare the solver
+        """
+        super().reset()
+        ### Generate the problem parameters
+        if self.env.optimisation_target == OptimisationTarget.MIN_COVER:
+            self._solver.set_problem_name("Minimum Vertex Cover")
+            self._solver.set_problem_type(cplex.Cplex.problem_type.LP)
+            self._solver.objective.set_sense(self._solver.objective.sense.minimize)
+            
+            names = [str(i) for i in range(self.env.n_spins)]
+            w_obj = [1] * self.env.n_spins
+            lb = [0] * self.env.n_spins
+            ub = [1] * self.env.n_spins
+
+            self._solver.variables.add(names=names, obj=w_obj, lb=lb, ub=ub)
+
+            all_int = [(var, self._solver.variables.type.integer) for var in names]
+            self._solver.variables.set_types(all_int)
+
+            constraints = []
+            # Go through each node and add a constraint for the edges in the upper triangular part of the adjacency matrix
+            for i in range(len(self.env.matrix)):
+                ### Start from i in each row so we don't double count edges
+                for j in range(i, len(self.env.matrix)):
+                    ### If there is an edges from i to j, add a constraint with their names and weight 1
+                    if self.env.matrix[i][j] == 1:
+                        constraints.append([[str(i),str(j)], [1, 1]])
+
+            # The names of the constrains, just being the string i,j
+            constraint_names = [",".join(x[0]) for x in constraints]
+            # Each edges must have at least one vertex
+            rhs = [1] * len(constraints)
+            # Constraint is always a lower limit, therefore "G"
+            constraint_senses = ["G"] * len(constraints)
+            self._solver.linear_constraints.add(names=constraint_names, 
+                                                 lin_expr=constraints, 
+                                                 senses=constraint_senses,
+                                                 rhs=rhs)
+
+    def solve(self):
+        print("Solving {} as a {} with cplex on graph {}".format(self._solver.get_problem_name(), self._solver.get_problem_type(), self.env))
+        self._solver.solve()
+        print("Solution result is {}".format(self._solver.solution.get_status_string()))
+        self.env.reset(2 * np.array(self._solver.solution.get_values()) - 1)
+
+    def step(self, *args):
+        """
+        Don't need step for this. cplex just solves instantly
+        """
+        return None
+    
+class NetworkXMinCoverSolver(SpinSolver):
+    def __init__(self, env: SpinSystemBase, record_cut=False, record_rewards=False, record_qs=False, verbose=False):
+        super().__init__(env, record_cut, record_rewards, record_qs, verbose)
+
+    def reset(self):
+        """
+        Reset the environment to empty
+        """
+        super().reset()
+        self.env.reset([-1] * self.env.n_spins)
+        self._solution = list(nx.algorithms.approximation.min_weighted_vertex_cover(nx.Graph(self.env.matrix)))
+        self._current_index = 0
+
+    def step(self):
+        """
+        Get the index of the current action to apply and do it on the environment
+        """
+        rew = 0
+        action = self._solution[self._current_index]
+        observation, reward, done, _ = self.env.step(action)
+        self._current_index += 1
+        if self._current_index == len(self._solution):
+            done = True
+
+        return rew, done
+        
