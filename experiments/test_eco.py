d47923a44f3d490b07eefd84ccff4f6e683f09ff
Dan <guitarman.dan29@gmail.com>
2023-06-22 06:46:01 -0400

    updated report, training and test updates

---
+++ experiments/test_eco.py
@@ -0,0 +1,326 @@
+import os
+import matplotlib.pyplot as plt
+import torch
+import numpy as np
+
+import sys
+sys.path.insert(0, '/home/cloudan/Documents/School/Project/eco-dqn')
+
+import src.envs.core as ising_env
+from experiments.utils import test_network, load_graph_set
+from src.envs.utils import (SingleGraphGenerator,
+                            RewardSignal, ExtraAction,
+                            OptimisationTarget, SpinBasis,
+                            MVC_OBSERVABLES, EdgeType, DEFAULT_OBSERVABLES)
+from src.networks.mpnn import MPNN
+from src.agents.solver import CplexSolver, CoverMatching, NetworkXMinCoverSolver, Greedy
+from src.envs.spinsystem import SpinSystemBase
+
+try:
+    import seaborn as sns
+    plt.style.use('seaborn')
+except ImportError:
+    pass
+
+def run(num_vertices, problem_type, graph_type, problem_params):
+    
+    save_loc = '{}_{}spin/eco/{}'.format(graph_type, num_vertices, problem_type)
+
+    print("\n----- Running {} -----\n".format(os.path.basename(__file__)))
+
+    ####################################################
+    # NETWORK LOCATION
+    ####################################################
+
+    data_folder = os.path.join(save_loc, 'data')
+    network_folder = os.path.join(save_loc, 'network')
+
+    print("data folder :", data_folder)
+    print("network folder :", network_folder)
+
+    network_save_path = os.path.join(network_folder, 'network_best.pth')
+
+    print("network params :", network_save_path)
+
+    ####################################################
+    # NETWORK SETUP
+    ####################################################
+
+    network_fn = MPNN
+    network_args = {
+        'n_layers': 3,
+        'n_features': 64,
+        'n_hid_readout': [],
+        'tied_weights': False
+    }
+
+    ####################################################
+    # SET UP ENVIRONMENTAL AND VARIABLES
+    ####################################################
+
+    step_factor = 2
+
+    env_args = {'observables': problem_params['observables'],
+                'reward_signal': RewardSignal.BLS,
+                'extra_action': ExtraAction.NONE,
+                'optimisation_target': problem_params['optimisation'],
+                'spin_basis': SpinBasis.SIGNED,
+                'norm_rewards': True,
+                'memory_length': None,
+                'horizon_length': None,
+                'stag_punishment': None,
+                'basin_reward': 1. / num_vertices,
+                'reversible_spins': True}
+
+    ####################################################
+    # LOAD VALIDATION GRAPHS
+    ####################################################
+
+    graph_sizes = [200]
+    all_graphs = []
+
+    ### TODO modify this to work with BA graphs as well
+    for i in graph_sizes:
+        all_graphs.append(load_graph_set("_graphs/validation/{}_{}spin_p15_100graphs.pkl".format(graph_type, i)))
+
+    ### CONVERT GRAPHS TO UNIFORM
+    if problem_params['edge_type'] == EdgeType.UNIFORM:
+        for diff_vert_count in all_graphs:
+            for i in range(len(diff_vert_count)):
+                diff_vert_count[i] = np.array(diff_vert_count[i] != 0, dtype=np.float64)
+    
+    
+    ####################################################
+    # SETUP NETWORK TO TEST
+    ####################################################
+
+    test_env = ising_env.make("SpinSystem",
+                              SingleGraphGenerator(all_graphs[0][0]),
+                              all_graphs[0][0].shape[0]*step_factor,
+                              **env_args)
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    torch.device(device)
+    print("Set torch default device to {}.".format(device))
+
+    network = network_fn(n_obs_in=test_env.observation_space.shape[1],
+                         **network_args).to(device)
+
+    network.load_state_dict(torch.load(network_save_path,map_location=device))
+    for param in network.parameters():
+        param.requires_grad = False
+    network.eval()
+
+    print("Sucessfully created agent with pre-trained MPNN.\nMPNN architecture\n\n{}".format(repr(network)))
+
+    batch_size = 50
+    solutions = {
+        'cplex_solutions': [],
+        'cover_matching_solutions': [],
+        'greedy_start_solutions': [],
+        'greedy_random_solutions': [],
+        'networkx_solutions': [],
+        'neural_network_empty_start_solutions': [],
+        'neural_network_full_start_solutions': [],
+        'neural_network_random_solutions': []
+    }
+
+    test_envs : list[SpinSystemBase] = [None] * batch_size
+
+    for i, graphs in enumerate(all_graphs):
+        cplex_batch = []
+        cover_matching_batch = []
+        greedy_start_batch = []
+        greedy_random_batch = []
+        networkx_batch = []
+        neural_network_empty_start_batch = []
+        neural_network_full_start_batch = []
+        neural_network_random_batch = []
+
+
+        for j, test_graph in enumerate(graphs):
+            env_args = {
+                'observables': problem_params['observables'],
+                'reward_signal': RewardSignal.BLS,
+                'extra_action': ExtraAction.NONE,
+                'optimisation_target': problem_params['optimisation'],
+                'spin_basis': SpinBasis.SIGNED,
+                'norm_rewards': True,
+                'memory_length': None,
+                'horizon_length': None,
+                'stag_punishment': None,
+                'basin_reward': 1. / test_graph.shape[0],
+                'reversible_spins': True
+                }
+            
+            # Create the environments that the solvers will run on
+            # For solvers that only need to be tested once, just use the first environment
+            print("Preparing batch of {} environments for graph {} with |V| = {}...".format(batch_size,j,test_graph.shape[0]))
+            for i in range(batch_size):
+                test_envs[i] = ising_env.make("SpinSystem",
+                                            SingleGraphGenerator(test_graph),
+                                            test_graph.shape[0]*step_factor,
+                                            **env_args)
+            
+
+            cplex_solver = CplexSolver(env=test_envs[0])
+            cplex_solver.reset()
+            cplex_solver.solve()
+            cplex_batch.append(test_envs[0].scorer.get_solution(test_envs[0].state[0, :test_envs[0].n_spins], test_envs[0].matrix))
+
+            # Next test cover matching (run 50 tests on each graph)
+            print("Running Matching Algorithm")
+            matching_solutions_batch = []
+            for i in range(batch_size):
+                matching_solver = CoverMatching(env=test_envs[i])
+                matching_solver.reset()
+                matching_solver.solve()
+                matching_solutions_batch.append(test_envs[i].scorer.get_solution(test_envs[i].state[0, :test_envs[i].n_spins], test_envs[i].matrix))
+
+            cover_matching_batch.append(matching_solutions_batch)
+
+            print("Running Greedy Algorithm from empty state")
+            # Next test greedy allowing reversal from start state
+            greedy_solver = Greedy(env=test_envs[0])
+            greedy_solver.reset()
+            greedy_solver.solve()
+            greedy_start_batch.append(test_envs[0].best_solution)
+
+            # Next test greedy allowing reversal from random state (run 50 tests on each graph)
+            print("Running Greedy Algorithm from random state")
+            greedy_random_sub_batch = []
+            for i in range(batch_size):
+                greedy_solver = Greedy(env=test_envs[i])
+                greedy_solver.reset()
+                greedy_solver.solve()
+                greedy_random_sub_batch.append(test_envs[i].best_solution)
+
+            greedy_random_batch.append(greedy_random_sub_batch)
+
+            print("Running NetworkX min_weighted_cover algorithm")
+            # Next test networkx minimum weighted cover
+            networkx_solver = NetworkXMinCoverSolver(env=test_envs[0])
+            networkx_solver.reset()
+            networkx_solver.solve()
+            networkx_batch.append(test_envs[i].scorer.get_solution(test_envs[i].state[0, :test_envs[i].n_spins], test_envs[0].matrix))
+
+            # Next test network from empty state
+            # First reset the environment to be empty, getting the observations
+            print("Running GECO on empty initial state.")
+            obs_batch = test_envs[0].reset([-1] * test_envs[0].n_spins)
+            done = False
+            while not done:
+                obs_batch = torch.FloatTensor(np.array(obs_batch)).to(device)
+                action = predict(network, obs_batch, test_envs[0].reversible_spins, test_envs[0].get_allowed_action_states())[0]
+                obs, rew, done, info = test_envs[0].step(action)
+                obs_batch = obs
+
+            # Once done, get best solution found
+            neural_network_empty_start_batch.append(test_envs[0].best_solution)
+
+            # Next test network from full state
+            print("Running GECO on full initial state")
+            obs_batch = test_envs[0].reset([1] * test_envs[0].n_spins)
+            done = False
+            while not done:
+                obs_batch = torch.FloatTensor(np.array(obs_batch)).to(device)
+                action = predict(network, obs_batch, test_envs[0].reversible_spins, test_envs[0].get_allowed_action_states())[0]
+                obs, rew, done, info = test_envs[0].step(action)
+                obs_batch = obs
+
+            # Once done, get best solution found
+            neural_network_full_start_batch.append(test_envs[0].best_solution)
+            
+            # Next test network from random state (run 50 tests on each graph)
+            print("Running GECO on random inital state.")
+            obs_batch = []
+            done = False
+            # Reset all the environments
+            for env in test_envs:
+                obs_batch.append(env.reset())
+
+            while not done:
+                obs_batch = torch.FloatTensor(np.array(obs_batch)).to(device)
+                # All envs in the batch have the same parameters, so pass them all to the predict
+                actions = predict(network, obs_batch, test_envs[0].reversible_spins, test_envs[0].get_allowed_action_states())
+
+                obs_batch = []
+                for env, action in zip(test_envs, actions):
+                    obs, rew, done, info = env.step(action)
+                    obs_batch.append(obs)
+
+            # Once done, add a list of every best solution to the solutions array
+            neural_network_random_batch.append([env.best_solution for env in test_envs])
+
+        solutions['cplex_solutions'].append(cplex_batch)
+        solutions['cover_matching_solutions'].append(cover_matching_batch)
+        solutions['greedy_start_solutions'].append(greedy_start_batch)
+        solutions['greedy_random_solutions'].append(greedy_random_batch)
+        solutions['networkx_solutions'].append(networkx_batch)
+        solutions['neural_network_empty_start_solutions'].append(neural_network_empty_start_batch)
+        solutions['neural_network_full_start_solutions'].append(neural_network_full_start_batch)
+        solutions['neural_network_random_solutions'].append(neural_network_random_batch)
+    
+    # Print this data to file
+    with open("test_data{}.txt".format(num_vertices), 'w') as f:
+        f.write(str(solutions))
+
+
+def run_with_params(num_vertices : int = 20, problem_type : str = 'min_cover', graph_type : str = 'ER'):
+    if problem_type == 'min_cover':
+        problem_params = {
+           'optimisation': OptimisationTarget.MIN_COVER,
+           'edge_type': EdgeType.UNIFORM,
+           'observables': MVC_OBSERVABLES
+        }
+    elif problem_type == 'max_cut':
+        problem_params = {
+            'optimisation': OptimisationTarget.CUT,
+            'edge_type': EdgeType.DISCRETE,
+            'observables': DEFAULT_OBSERVABLES
+        }
+    elif problem_type == 'min_cut':
+        problem_params = {
+            'optimisation': OptimisationTarget.MIN_CUT,
+            'edge_type': EdgeType.DISCRETE,
+            'observables': DEFAULT_OBSERVABLES
+        }
+    else:
+        print('Invalid problem type.')
+        exit(1)
+
+    run(num_vertices, problem_type, graph_type, problem_params)
+
+
+def predict(network, states, acting_in_reversible_spin_env, allowed_action_state):
+    """
+    Given a network and environment states, gives actions for each environment. Because the network is on GPU, this allows multiple 
+    environments to be evaluated simultaneously.
+
+    TODO: MOVE THIS TO A SOLVER THAT CAN SOLVE MULTIPLE GRAPHS AT ONCE
+    """
+
+    qs = network(states)
+
+    if acting_in_reversible_spin_env:
+        if qs.dim() == 1:
+            actions = [qs.argmax().item()]
+        else:
+            actions = qs.argmax(1, True).squeeze(1).cpu().numpy()
+        return actions
+    else:
+        if qs.dim() == 1:
+            x = (states.squeeze()[:,0] == allowed_action_state).nonzero()
+            actions = [x[qs[x].argmax().item()].item()]
+        else:
+            disallowed_actions_mask = (states[:, :, 0] != allowed_action_state)
+            qs_allowed = qs.masked_fill(disallowed_actions_mask, np.finfo(np.float64).min)
+            actions = qs_allowed.argmax(1, True).squeeze(1).cpu().numpy()
+        return actions
+
+
+if __name__ == "__main__":
+    num_vertices = 20
+    graph_type = 'ER'
+    problem_type = 'min_cover'
+    run_with_params(num_vertices, problem_type, graph_type)
---
+++ experiments/train_eco.py
@@ -0,0 +1,341 @@
+import os
+import pickle
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+import sys
+sys.path.insert(0, '/home/cloudan/Documents/School/Project/eco-dqn')
+
+import src.envs.core as ising_env
+from experiments.utils import load_graph_set, mk_dir
+from src.agents.dqn.dqn import DQN
+from src.agents.dqn.utils import TestMetric
+from src.envs.utils import (SetGraphGenerator,
+                            RandomErdosRenyiGraphGenerator,
+                            EdgeType, RewardSignal, ExtraAction,
+                            OptimisationTarget, SpinBasis,
+                            MVC_OBSERVABLES, DEFAULT_OBSERVABLES,
+                            RandomBarabasiAlbertGraphGenerator)
+from src.networks.mpnn import MPNN
+
+try:
+    import seaborn as sns
+    plt.style.use('seaborn')
+except ImportError:
+    pass
+
+import time
+
+def run(num_vertices, problem_type, dqn_params, problem_params, graph_type, train_graph_generator):
+    save_loc = "{}_{}spin/eco/{}".format(graph_type, num_vertices, problem_type)
+
+    print("\n----- Running {} -----\n".format(os.path.basename(__file__)))
+
+    ####################################################
+    # SET UP ENVIRONMENTAL AND VARIABLES
+    ####################################################
+
+    gamma=0.95
+    step_fact = 2
+
+    env_args = {'observables':problem_params['observables'],
+                'reward_signal':RewardSignal.BLS,
+                'extra_action':ExtraAction.NONE,
+                'optimisation_target':problem_params['optimisation'],
+                'spin_basis':SpinBasis.SIGNED,
+                'norm_rewards':True,
+                'memory_length':None,
+                'horizon_length':None,
+                'stag_punishment':None,
+                'basin_reward':1./num_vertices,
+                'reversible_spins':True}
+
+    ####################################################
+    # SET UP TRAINING AND TEST GRAPHS
+    ####################################################
+
+    ####
+    # Pre-generated test graphs
+    ####
+    graph_save_loc = "_graphs/testing/{}_{}spin_p15_50graphs.pkl".format(graph_type, num_vertices)
+    graphs_test = load_graph_set(graph_save_loc)
+    n_tests = len(graphs_test)
+
+    # If we want uniform edges... instead of generating new graphs, take the discrete test graphs and make them uniform
+    if problem_params['edge_type'] == EdgeType.UNIFORM:
+        for i in range(len(graphs_test)):
+            graphs_test[i] = np.array(graphs_test[i] != 0, dtype=np.float64)
+
+    test_graph_generator = SetGraphGenerator(graphs_test, ordered=True)
+
+    ####################################################
+    # SET UP TRAINING AND TEST ENVIRONMENTS
+    ####################################################
+
+    train_envs = [ising_env.make("SpinSystem",
+                                 train_graph_generator,
+                                 int(num_vertices*step_fact),
+                                 **env_args)]
+
+
+    n_spins_test = train_graph_generator.get().shape[0]
+    test_envs = [ising_env.make("SpinSystem",
+                                test_graph_generator,
+                                int(n_spins_test*step_fact),
+                                **env_args)]
+
+    ####################################################
+    # SET UP FOLDERS FOR SAVING DATA
+    ####################################################
+
+    data_folder = os.path.join(save_loc,'data')
+    network_folder = os.path.join(save_loc, 'network')
+
+    mk_dir(data_folder)
+    mk_dir(network_folder)
+    
+    network_save_path = os.path.join(network_folder,'network.pth')
+    test_save_path = os.path.join(network_folder,'test_scores.pkl')
+    loss_save_path = os.path.join(network_folder, 'losses.pkl')
+    solutions_save_path = os.path.join(network_folder, 'solution.pkl')
+
+    ####################################################
+    # SET UP AGENT
+    ####################################################
+
+    nb_steps = dqn_params['num_steps']
+
+    network_fn = lambda: MPNN(n_obs_in=train_envs[0].observation_space.shape[1],
+                              n_layers=3,
+                              n_features=64,
+                              n_hid_readout=[],
+                              tied_weights=False)
+
+    agent = DQN(train_envs,
+
+                network_fn,
+
+                init_network_params=None,
+                init_weight_std=0.01,
+
+                double_dqn=True,
+                clip_Q_targets=False,
+
+                replay_start_size=dqn_params['replay_start_size'],
+                replay_buffer_size=dqn_params['replay_buffer_size'],  # 20000
+                gamma=gamma,  # 1
+                update_target_frequency=dqn_params['update_target_frequency'],  # 500
+
+                update_learning_rate=False,
+                initial_learning_rate=1e-4,
+                peak_learning_rate=1e-4,
+                peak_learning_rate_step=20000,
+                final_learning_rate=1e-4,
+                final_learning_rate_step=200000,
+
+                update_frequency=32,  # 1
+                minibatch_size=64,  # 128
+                max_grad_norm=None,
+                weight_decay=0,
+
+                update_exploration=True,
+                initial_exploration_rate=1,
+                final_exploration_rate=0.05,  # 0.05
+                final_exploration_step=dqn_params['final_exploration_step'],  # 40000
+
+                adam_epsilon=1e-8,
+                logging=False,
+                loss="mse",
+
+                save_network_frequency=dqn_params['save_network_frequency'],
+                network_save_path=network_save_path,
+
+                evaluate=True,
+                test_envs=test_envs,
+                test_episodes=n_tests,
+                test_frequency=dqn_params['test_frequency'],  # 10000
+                test_save_path=test_save_path,
+                test_metric=TestMetric.BEST,
+
+                seed=None
+                )
+
+    print("\n Created DQN agent with network:\n\n", agent.network)
+
+    #############
+    # TRAIN AGENT
+    #############
+    start = time.time()
+    agent.learn(timesteps=nb_steps, verbose=True)
+    print(time.time() - start)
+
+    agent.save()
+
+    ############
+    # PLOT - solution curve
+    ############
+    data = pickle.load(open(solutions_save_path,'rb'))
+    data = np.array(data)
+
+    fig_fname = os.path.join(network_folder,"training_curve")
+
+    plt.plot(data[:,0],data[:,1])
+    plt.xlabel("Timestep")
+    plt.ylabel("Mean Solution Quality")
+    if agent.test_metric == TestMetric.FINAL:
+       plt.ylabel("Mean final solution")
+    if agent.test_metric==TestMetric.ENERGY_ERROR:
+      plt.ylabel("Energy Error")
+    elif agent.test_metric==TestMetric.CUMULATIVE_REWARD:
+      plt.ylabel("Cumulative Reward")
+
+    plt.savefig(fig_fname + ".png", bbox_inches='tight')
+    plt.savefig(fig_fname + ".pdf", bbox_inches='tight')
+
+    plt.clf()
+
+    ############
+    # PLOT - score curve
+    ############
+    data = pickle.load(open(test_save_path,'rb'))
+    data = np.array(data)
+
+    fig_fname = os.path.join(network_folder,"score_curve")
+
+    plt.plot(data[:,0],data[:,1])
+    plt.xlabel("Timestep")
+    plt.ylabel("Mean score")
+    if agent.test_metric == TestMetric.FINAL:
+       plt.ylabel("Mean final score")
+    if agent.test_metric==TestMetric.ENERGY_ERROR:
+      plt.ylabel("Energy Error")
+    elif agent.test_metric==TestMetric.CUMULATIVE_REWARD:
+      plt.ylabel("Cumulative Reward")
+
+    plt.savefig(fig_fname + ".png", bbox_inches='tight')
+    plt.savefig(fig_fname + ".pdf", bbox_inches='tight')
+
+    plt.clf()
+
+    ############
+    # PLOT - losses
+    ############
+    data = pickle.load(open(loss_save_path,'rb'))
+    data = np.array(data)
+
+    fig_fname = os.path.join(network_folder,"loss")
+
+    N=50
+    data_x = np.convolve(data[:,0], np.ones((N,))/N, mode='valid')
+    data_y = np.convolve(data[:,1], np.ones((N,))/N, mode='valid')
+
+    plt.plot(data_x,data_y)
+    plt.xlabel("Timestep")
+    plt.ylabel("Loss")
+
+    plt.yscale("log")
+    plt.grid(True)
+
+    plt.savefig(fig_fname + ".png", bbox_inches='tight')
+    plt.savefig(fig_fname + ".pdf", bbox_inches='tight')
+
+
+
+def run_with_vars(num_vertices=20, problem_type='min_cover', graph_type='ER'):
+    if problem_type == 'min_cover':
+        problem_params = {
+           'optimisation': OptimisationTarget.MIN_COVER,
+           'edge_type': EdgeType.UNIFORM,
+           'observables': MVC_OBSERVABLES
+        }
+    elif problem_type == 'max_cut':
+        problem_params = {
+            'optimisation': OptimisationTarget.CUT,
+            'edge_type': EdgeType.DISCRETE,
+            'observables': DEFAULT_OBSERVABLES
+        }
+    elif problem_type == 'min_cut':
+        problem_params = {
+            'optimisation': OptimisationTarget.MIN_CUT,
+            'edge_type': EdgeType.DISCRETE,
+            'observables': DEFAULT_OBSERVABLES
+        }
+    else:
+        print('Invalid problem type.')
+        exit(1)
+
+    if graph_type == 'ER':
+        graph_generator = RandomErdosRenyiGraphGenerator(
+            num_vertices, 
+            0.15, 
+            problem_params['edge_type']
+        )
+    elif graph_type == 'BA':
+        graph_generator = RandomBarabasiAlbertGraphGenerator(
+           num_vertices,
+           4,
+           problem_params['edge_type']
+        )
+    else:
+        print('Invalid graph generator type.')
+        exit(1)
+
+    if num_vertices == 20 or num_vertices == 40:
+        dqn_params = {
+            'num_steps': 2500000,
+            'replay_start_size': 500,
+            'replay_buffer_size': 5000,
+            'update_target_frequency': 1000,
+            'final_exploration_step': 150000,
+            'save_network_frequency': 100000,
+            'test_frequency': 10000
+        }
+    elif num_vertices == 60:
+        dqn_params = {
+            'num_steps': 5000000,
+            'replay_start_size': 500,
+            'replay_buffer_size': 5000,
+            'update_target_frequency': 1000,
+            'final_exploration_step': 300000,
+            'save_network_frequency': 200000,
+            'test_frequency': 20000
+        }
+    elif num_vertices == 100:
+        dqn_params = {
+            'num_steps': 8000000,
+            'replay_start_size': 1500,
+            'replay_buffer_size': 10000,
+            'update_target_frequency': 2500,
+            'final_exploration_step': 800000,
+            'save_network_frequency': 400000,
+            'test_frequency': 50000
+        }
+    elif num_vertices == 200:
+        dqn_params = {
+            'num_steps': 10000000,
+            'replay_start_size': 3000,
+            'replay_buffer_size': 15000,
+            'update_target_frequency': 4000,
+            'final_exploration_step': 800000,
+            'save_network_frequency': 400000,
+            'test_frequency': 50000
+        }
+    else:
+        print('Undefined vertex number.')
+        exit(1)
+
+    run(
+        num_vertices, 
+        problem_type, 
+        dqn_params, 
+        problem_params, 
+        graph_type,
+        graph_generator
+    )
+
+if __name__ == "__main__":
+    num_vertices = 20
+    problem_type = 'min_cover'
+    graph_type = 'ER'
+    run_with_vars(num_vertices, problem_type, graph_type)
